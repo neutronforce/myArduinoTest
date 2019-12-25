f21891abd70ff6ac8cc7ade836532e7d19fc1c17
Andres Altamirano <a@altafly.com>
2019-12-25 16:08:50 -0600

    set up conditions for all characters in the alphabet

--- morse2/morse2.ino
+++ morse2/morse2.ino
@@ -18,18 +18,20 @@
 #define DELAY 50
 #define DOT '.'
 #define DASH '-'
+#define NIL '\0'
+#define MAX_MORSE 5
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
-char morse[5];
+char morse[MAX_MORSE] = {NIL, NIL, NIL, NIL, NIL};
 uint8_t mPos = 0;
-unsigned short signalLength = 0;
-//short hue = 0;
+unsigned long signalStart = 0;
+short hue = 0;
 bool dotWasPushed = false;
 bool dashWasPushed = false;
 
 void setup() {
- // Serial.begin(9600);
+  Serial.begin(9600);
   matrix.begin();
   matrix.fillScreen(matrix.Color333(0, 0, 0));
   matrix.setCursor(0, 0);
@@ -39,141 +41,180 @@ void setup() {
   pinMode(DOTB, INPUT_PULLUP);
   pinMode(DASHB, INPUT_PULLUP);
   pinMode(JOYB, INPUT_PULLUP);
+
+  digitalWrite(DOTB, HIGH);
+  digitalWrite(DASHB, HIGH);
+  digitalWrite(JOYB, HIGH);
 }
 
 void loop() {
   checkMorse();
-  delay(1);
 }
 
 void checkMorse() {
+
   boolean dotPushed = (LOW == digitalRead(DOTB));
   boolean dashPushed = (LOW == digitalRead(DASHB));
+  //  if (dotPushed) Serial.println(" .dot btn pushed. ");
+  //  if (dashPushed) Serial.println(" -dash btn pushed- ");
 
-  if ((dotPushed && dotWasPushed) || (dashPushed && dashWasPushed)) { //button still pushed
-    if (signalLength < USHRT_MAX) {
-      signalLength++;
-    }
+  if ((dotPushed && dotWasPushed) || (dashPushed && dashWasPushed)) {
+    //    Serial.println("button still pushed");
   }
-  else if ((!dotPushed && dotWasPushed) || (!dashPushed && dashWasPushed)) { //button released
-    if (signalLength > DELAY) {
+  else if ((!dotPushed && dotWasPushed) || (!dashPushed && dashWasPushed)) {
+    //    Serial.println("button released");
+    if ((millis() - signalStart) > DELAY) {
       if (dotWasPushed) {
-   //     Serial.println("DOT");
+        //        Serial.println("DOT");
         morse[mPos++] = DOT;
       }
       else if (dashWasPushed) {
-   //     Serial.println("DASH");
+        //        Serial.println("DASH");
         morse[mPos++] = DASH;
       }
     }
-
+    //Serial.print("Morse:" ); Serial.println(morse);
+    //Serial.print("mPos:" ); Serial.println(mPos);
   }
-  else if ((dotPushed && !dotWasPushed) || (dashPushed && !dashWasPushed)) // button pushed
+  else if ((dotPushed && !dotWasPushed) || (dashPushed && !dashWasPushed))
   {
-    signalLength = 0;
+    //Serial.println("button pushed");
+    signalStart = millis();
   }
-  else if ((!dotPushed && !dotWasPushed) && (!dashPushed && !dashWasPushed))  //button not pushed
+  else if ((!dotPushed && !dotWasPushed) && (!dashPushed && !dashWasPushed))
   {
-    if (signalLength < USHRT_MAX) {
-      signalLength++;
-    }
-    if (mPos == 5 || (( signalLength > PAUSE ) && (mPos > 0)))
+    if (mPos == MAX_MORSE || (((millis() - signalStart) > PAUSE ) && (mPos > 0)))
     {
       readMorse(morse);
       mPos = 0;
-      for (int i = 0; i < 5; i++) morse[i] = ' ';
+      for (int i = 0; i < MAX_MORSE; i++) morse[i] = NIL;
+      //Serial.print("Morse after clearing:" ); Serial.println(morse);
     }
   }
-  //if(dotPushed) Serial.print(" .dot btn pushed. ");
-  //if(dashPushed) Serial.print(" -dash btn pushed- ");
   dotWasPushed = dotPushed;
   dashWasPushed = dashPushed;
 }
 
 void appendChar(char c) {
- // matrix.setTextColor(matrix.ColorHSV(hue, 255, 255, true));
+  //Serial.print("Append to screen: ");  Serial.println(c);
+  matrix.setTextColor(matrix.ColorHSV(hue, 255, 255, true));
   matrix.print(c);
- // hue += 7; if (hue >= 1536) hue -= 1536;
+  hue += 7; if (hue >= 1536) hue -= 1536;
 }
 
 void readMorse(char str[])
 {
-//  Serial.print("Decoding:"); Serial.println(str);
-  if (str[0] == DOT) {
-    if (str[1] == DASH) {
-      appendChar('A');
-    }
+  //Serial.print("Decoding: ");
+  Serial.println(str);
+  if (str[0] == DOT && str[1] == DASH && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('A');
   }
-
-  //  else if (str == "-***")
-  //    appendChar("B");
-  //  else if (str == "-*-*")
-  //    appendChar("C");
-  //  else if (str == "-**")
-  //    appendChar("D");
-  //  else if (str == "*")
-  //    appendChar("E");
-  //  else if (str == "**-*")
-  //    appendChar("F");
-  //  else if (str == "--*")
-  //    appendChar("G");
-  //  else if (str == "****")
-  //    appendChar("H");
-  //  else if (str == "**")
-  //    appendChar("I");
-  //  else if (str == "*---")
-  //    appendChar("J");
-  //  else if (str == "-*-")
-  //    appendChar("K");
-  //  else if (str == "*-**")
-  //    appendChar("L");
-  //  else if (str == "--")
-  //    appendChar("M");
-  //  else if (str == "-*")
-  //    appendChar("N");
-  //  else if (str == "---")
-  //    appendChar("O");
-  //  else if (str == "*--*")
-  //    appendChar("P");
-  //  else if (str == "--*-")
-  //    appendChar("Q");
-  //  else if (str == "*-*")
-  //    appendChar("R");
-  //  else if (str == "***")
-  //    appendChar("S");
-  //  else if (str == "-")
-  //    appendChar("T");
-  //  else if (str == "**-")
-  //    appendChar("U");
-  //  else if (str == "***-")
-  //    appendChar("V");
-  //  else if (str == "*--")
-  //    appendChar("W");
-  //  else if (str == "-**-")
-  //    appendChar("X");
-  //  else if (str == "-*--")
-  //    appendChar("Y");
-  //  else if (str == "--**")
-  //    appendChar("Z");
-  //
-  //  else if (str == "*----")
-  //    appendChar("1");
-  //  else if (str == "**---")
-  //    appendChar("2");
-  //  else if (str == "***--")
-  //    appendChar("3");
-  //  else if (str == "****-")
-  //    appendChar("4");
-  //  else if (str == "*****")
-  //    appendChar("5");
-  //  else if (str == "-****")
-  //    appendChar("6");
-  //  else if (str == "--***")
-  //    appendChar("7");
-  //  else if (str == "---**")
-  //    appendChar("8");
-  //  else if (str == "----*")
-  //    appendChar("9");
-  //  else if (str == "-----")
-  //    appendChar("0");
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DOT && str[3] == DOT && str[4] == NIL) {
+    appendChar('B');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DASH && str[3] == DOT && str[4] == NIL) {
+    appendChar('C');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DOT && str[3] == NIL && str[4] == NIL) {
+    appendChar('D');
+  }
+  else if (str[0] == DOT && str[1] == NIL && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('E');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DASH && str[3] == DOT && str[4] == NIL) {
+    appendChar('F');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DOT && str[3] == NIL && str[4] == NIL) {
+    appendChar('G');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == DOT && str[4] == NIL) {
+    appendChar('H');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('I');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DASH && str[3] == DASH && str[4] == NIL) {
+    appendChar('J');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DASH && str[3] == NIL && str[4] == NIL) {
+    appendChar('K');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DOT && str[3] == DOT && str[4] == NIL) {
+    appendChar('L');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('M');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('N');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DASH && str[3] == NIL && str[4] == NIL) {
+    appendChar('O');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DASH && str[3] == DOT && str[4] == NIL) {
+    appendChar('P');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DOT && str[3] == DASH && str[4] == NIL) {
+    appendChar('Q');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DOT && str[3] == NIL && str[4] == NIL) {
+    appendChar('R');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == NIL && str[4] == NIL) {
+    appendChar('S');
+  }
+  else if (str[0] == DASH && str[1] == NIL && str[2] == NIL && str[3] == NIL && str[4] == NIL) {
+    appendChar('T');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DASH && str[3] == NIL && str[4] == NIL) {
+    appendChar('U');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == DASH && str[4] == NIL) {
+    appendChar('V');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DASH && str[3] == NIL && str[4] == NIL) {
+    appendChar('W');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DOT && str[3] == DASH && str[4] == NIL) {
+    appendChar('X');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DASH && str[3] == DASH && str[4] == NIL) {
+    appendChar('Y');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DOT && str[3] == DOT && str[4] == NIL) {
+    appendChar('Z');
+  }
+  else if (str[0] == DOT && str[1] == DASH && str[2] == DASH && str[3] == DASH && str[4] == DASH) {
+    appendChar('1');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DASH && str[3] == DASH && str[4] == DASH) {
+    appendChar('2');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == DASH && str[4] == DASH) {
+    appendChar('3');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == DOT && str[4] == DASH) {
+    appendChar('4');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DOT && str[3] == DOT && str[4] == DOT) {
+    appendChar('5');
+  }
+  else if (str[0] == DASH && str[1] == DOT && str[2] == DOT && str[3] == DOT && str[4] == DOT) {
+    appendChar('6');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DOT && str[3] == DOT && str[4] == DOT) {
+    appendChar('7');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DASH && str[3] == DOT && str[4] == DOT) {
+    appendChar('8');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DASH && str[3] == DASH && str[4] == DOT) {
+    appendChar('9');
+  }
+  else if (str[0] == DASH && str[1] == DASH && str[2] == DASH && str[3] == DASH && str[4] == DASH) {
+    appendChar('0');
+  }
+  else if (str[0] == DOT && str[1] == DOT && str[2] == DASH && str[3] == DASH && str[4] == NIL) {
+    appendChar(' ');
+  }
 }
